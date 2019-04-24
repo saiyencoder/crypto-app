20bf880bf4f7ef2226bae4c893b7d67c3f05c275
Jesus Santizo <jsantizo@selectrehab.com>
2019-04-24 13:23:24 -0500

    Implemented search functionality to search for curreny symbols

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,7 @@
 class HomeController < ApplicationController
+  before_action :api_data, only: [:index, :search]
   
   def index
-    response = HTTParty.get(
-                             BASE_URL, 
-                             query: 
-                                    {
-                                      "CMC_PRO_API_KEY": "#{COIN_KEY}"
-                                    }
-                            ).to_json
-    @coins = JSON.parse(response)
-    @data = @coins["data"]
     @my_coins = ["BTC","XRP","ADA","XLM", "STEEM"]
   end
 
@@ -27,4 +19,19 @@ class HomeController < ApplicationController
       @symbol = "Please enter a currency to search"
     end
   end
+
+  private
+  
+  def api_data
+    response = HTTParty.get(
+                             BASE_URL, 
+                             query: 
+                                    {
+                                      "CMC_PRO_API_KEY": "#{COIN_KEY}"
+                                    }
+                            ).to_json
+    @coins = JSON.parse(response)
+    @data = @coins["data"]
+  end
+
 end
