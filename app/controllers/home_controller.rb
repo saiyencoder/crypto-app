bb42e7241130dc17f988d5f4493fff753b237190
Jesus Santizo <jsantizo@selectrehab.com>
2019-05-03 13:19:31 -0500

    Cleaned up search action in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,32 +12,21 @@ class HomeController < ApplicationController
   def search
     @symbol = params[:sym]
 
-    # if @symbol == ""
-    #   # This helps the flash msg not to appear when search is empty
-    #   flash.now[:notice] = "Please enter a currency to search" if params[:searching]
-    # end
-
     if @symbol
-      @symbol = validate_search(@symbol.upcase)
-
-      if @symbol 
-        @symbol = @symbol.first
+      @symbol.upcase!
+      # If search is empty
+      if @symbol == ""
+        flash.now[:notice] = "Please enter a currency to search" if params[:searching]
       else
-        flash.now[:notice] = "Invalid entry. Please try again"
+        found_symbol = @data.select{|key| key["symbol"] == @symbol}
+        # If search is not a valid symbol
+        if found_symbol.empty?
+          @symbol = false
+          flash.now[:notice] = "Invalid entry. Please try again"
+        else
+          @symbol = found_symbol.first
+        end
       end
-    end
-
-  end
-
-  private
-
-  def validate_search(symbol)
-    found_symbol = @data.select{|key,value| key["symbol"] == symbol}
-
-    if found_symbol.empty?
-      false
-    else
-      found_symbol
     end
   end
 
