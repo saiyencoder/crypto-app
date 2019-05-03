941de0394831880e2816cda942ec43fb5cb880a0
Jesus Santizo <jsantizo@selectrehab.com>
2019-05-02 22:42:38 -0500

    Updated/cleaned search function in controller. Still needs more work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,15 +12,33 @@ class HomeController < ApplicationController
   def search
     @symbol = params[:sym]
 
-    if @symbol == ""
-      # This helps the flash msg not to appear when search is empty
-      flash.now[:notice] = "Please enter a currency to search" if params[:searching]
-    end
+    # if @symbol == ""
+    #   # This helps the flash msg not to appear when search is empty
+    #   flash.now[:notice] = "Please enter a currency to search" if params[:searching]
+    # end
 
     if @symbol
-      @symbol = @symbol.upcase 
+      @symbol = validate_search(@symbol.upcase)
+
+      if @symbol 
+        @symbol = @symbol.first
+      else
+        flash.now[:notice] = "Invalid entry. Please try again"
+      end
     end
 
+  end
+
+  private
+
+  def validate_search(symbol)
+    found_symbol = @data.select{|key,value| key["symbol"] == symbol}
+
+    if found_symbol.empty?
+      false
+    else
+      found_symbol
+    end
   end
 
 end
